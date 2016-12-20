1c2304b57b170516c86cdec83f7ec5aba568f033
Olya Olya <achendina.com>
2016-12-20 13:40:43 +0300

    PORTFOLIO section gallery has been connected

--- js/main.js
+++ js/main.js
@@ -46,6 +46,18 @@ $(document).ready(function() {
 	//    highlightSelector:"nav a"
 	//});
 
+    // MixItUp - фильтрация работ в портфолио
 	$('#portfolio-projects').mixItUp();	
+
+	// FancyBox - galery
+	$(".fancybox").fancybox({
+			// Default - with fix from scroll to top
+            helpers: {
+                overlay: {
+                    locked: false
+                }
+            }
+    });
+	// End of FancyBox - galery
 
 }); 
