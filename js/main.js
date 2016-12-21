d62a89bcad6df542f02921a819976dd5113a76d7
Olya Olya <achendina.com>
2016-12-21 12:18:54 +0300

    slide2id is on

--- js/main.js
+++ js/main.js
@@ -42,9 +42,9 @@ $(document).ready(function() {
 	})
 
 //slide2id - плавная прокрутка по ссылкам внутри страницы
-	//$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-	//    highlightSelector:"nav a"
-	//});
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 
     // MixItUp - фильтрация работ в портфолио
 	$('#portfolio-projects').mixItUp();	
