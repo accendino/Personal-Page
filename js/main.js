90e2916707f07f2fc604c42a8b13234befd9bdba
Olya Olya <achendina.com>
2016-12-22 19:51:35 +0300

    NAVicon is animated

--- js/main.js
+++ js/main.js
@@ -1,42 +1,70 @@
 $(document).ready(function() {
 
-    /*перем. доделать*/
-	var navToggle = $('.navigation__toggle');
+	//var toggle = $("#navigation__button")
+
+    //toggle.on("click", function(e){
+    //e.preventDefault();
+    //if (toggle.hasClass("active")) {
+    // toggle.removeClass("active")
+    //} else {
+    //  toggle.addClass("active")
+    //}
+    //})
+
+    /*переменные*/
+	var navToggle = $('#navigation__button');
 	var navIcon = $('.navigation__toggle .fa');
 	var navBlock = $('.navigation__list');
 	var navBlockOpen = 'navigation__list--open';
 	var navLink = $('.navigation__list a');
-	var iconNav = 'fa-bars';
-	var iconClose = 'fa-times';
+	//var iconOpen = 'fa-bars';
+	//var iconClose = 'fa-times';
 
 	/*menu mobile*/
-	$('.navigation__toggle').on('click',function(e) {
+	navToggle.on('click',function(e) {
 		e.preventDefault();
-		$('.navigation__list').toggleClass('navigation__list--open');
+		navBlock.toggleClass(navBlockOpen);
 
-		if ($('.navigation__toggle .fa').hasClass('fa-bars')) {
-			$('.navigation__toggle .fa').removeClass('fa-bars');
-		    $('.navigation__toggle .fa').addClass('fa-times');
-		} 
-		else {
-			$('.navigation__toggle .fa').removeClass('fa-times');
-		    $('.navigation__toggle .fa').addClass('fa-bars');
-		}		
+		if (navToggle.hasClass("active")) {
+        navToggle.removeClass("active");
+
+        } else {
+        navToggle.addClass("active");
+        }
+
+		//if (navIcon.hasClass(iconOpen)) {
+		//	navIcon.removeClass(iconOpen);
+		//    navIcon.addClass(iconClose);
+		//} 
+		//else {
+		//	navIcon.removeClass(iconClose);
+		//    navIcon.addClass(iconOpen);
+		//}		
 
 	})
 
 	navLink.on('click',function() {
 
-		if ( navBlock.hasClass(navBlockOpen) ) {
-			if ( navIcon.hasClass(iconNav) ){
-				navIcon.removeClass(iconNav);
-				navIcon.addClass(iconClose);
-			} else {
-				navIcon.removeClass(iconClose);
-				navIcon.addClass(iconNav);
-			}
-		}
+		if (navBlock.hasClass(navBlockOpen)){
 
+		if (navToggle.hasClass("active")) {
+        navToggle.removeClass("active");
+
+        } else {
+        navToggle.addClass("active");
+        }
+    }
+
+		//if ( navBlock.hasClass(navBlockOpen) ) {
+		//	if ( navIcon.hasClass(iconOpen) ){
+		//		navIcon.removeClass(iconOpen);
+		//		navIcon.addClass(iconClose);
+		//	} else {
+		//		navIcon.removeClass(iconClose);
+		//		navIcon.addClass(iconOpen);
+		//	}
+		//}
+//
 		navBlock.removeClass(navBlockOpen);
 		
 	})
