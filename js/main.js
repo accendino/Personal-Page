f0be8d973e67bce2b725a3858092faa471ed6d89
Olya Olya <achendina.com>
2016-12-23 01:43:33 +0300

    1. Submission form has been configured; 2. Download button has been configured; 3. Header background-size has been changed

--- js/main.js
+++ js/main.js
@@ -78,7 +78,37 @@ $(document).ready(function() {
 			//skype: "",
 			//phone: "",
 			message: "Пожалуйста, введите текст сообщения",
+		},
+
+		submitHandler: function(form) {
+		  // do other things for a valid form
+		  formAjaxSubmit();
 		}
 	})
+
+	// Скрипт AJAX запрса на сервер
+
+	// По сабмиту формы делаем следующие действия
+
+	function formAjaxSubmit() {
+
+		var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+		// Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+			
+			// Функция если все прошло успешно
+			success: function(html){
+				$("#contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false; 
+	}
 
 }); 
